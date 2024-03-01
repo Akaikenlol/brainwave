5b20941496c95725cd753b0e872067a26e08e4b9
Akaikenlol <keithken972@gmail.com>
2024-03-01 21:08:59 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+
+const App = () => {
+	return (
+		<>
+			<div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+				<Header />
+			</div>
+			<ButtonGradient />
+		</>
+	);
+};
+
+export default App;
