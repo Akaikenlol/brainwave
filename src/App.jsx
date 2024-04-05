61f6f4f5f264904cf4fbeb3d2bf6a03f4b02b1ad
Akaikenlol <keithken972@gmail.com>
2024-04-05 21:35:25 +0630

    feat: Benefits component, Heading component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
 
 const App = () => {
 	return (
@@ -9,6 +10,7 @@ const App = () => {
 			<div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
 				<Header />
 				<Hero />
+				<Benefits />
 			</div>
 			<ButtonGradient />
 		</>
